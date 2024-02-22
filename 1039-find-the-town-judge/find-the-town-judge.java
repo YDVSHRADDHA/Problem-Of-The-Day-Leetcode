c60bbf0d9eece55c0e7c8c994ca7364a5377f9e6
Shraddha Yadav <97733987+YDVSHRADDHA@users.noreply.github.com>
2024-02-23 00:59:45 +0530

    Time: 27 ms (7.72%) | Memory: 52.3 MB (82.99%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,19 +1,20 @@
 import java.util.HashMap;
 import java.util.Map;
+
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if(n==1 && trust.length ==0){
+        if (n == 1 && trust.length == 0) {
             return 1;
         }
-        Map<Integer,Integer> inDegree = new HashMap<>();
-        Map<Integer,Integer> outDegree = new HashMap<>();
+        Map<Integer, Integer> inDegree = new HashMap<>();
+        Map<Integer, Integer> outDegree = new HashMap<>();
 
-       for (int[] relationship : trust) {
+        for (int[] relationship : trust) {
             outDegree.put(relationship[0], outDegree.getOrDefault(relationship[0], 0) + 1);
             inDegree.put(relationship[1], inDegree.getOrDefault(relationship[1], 0) + 1);
         }
 
-        for(int i =1; i<=n; i++){
+        for (int i = 1; i <= n; i++) {
             if (inDegree.getOrDefault(i, 0) == n - 1 && !outDegree.containsKey(i)) {
                 return i;
             }
