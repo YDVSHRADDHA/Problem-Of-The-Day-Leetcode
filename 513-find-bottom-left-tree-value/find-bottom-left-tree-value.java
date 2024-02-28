4998eeb534d0397dd31ad902c64c1af0032f6cfd
Shraddha Yadav <97733987+YDVSHRADDHA@users.noreply.github.com>
2024-02-29 03:26:42 +0530

    Time: 1 ms (69.74%) | Memory: 44.3 MB (41.94%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -14,27 +14,45 @@
  * }
  */
 class Solution {
-    private int maxDepth;
-    private int bottomLeftValue;
-    public int findBottomLeftValue(TreeNode root) {
-        // DFS
-        maxDepth = -1;
-        bottomLeftValue = 0;
-        dfs(root,0);
+    // private int maxDepth;
+    // private int bottomLeftValue;
+    // public int findBottomLeftValue(TreeNode root) {
+    //     // DFS
+    //     maxDepth = -1;
+    //     bottomLeftValue = 0;
+    //     dfs(root,0);
 
-        return bottomLeftValue;
-    }
+    //     return bottomLeftValue;
+    // }
 
-    private void dfs(TreeNode current, int depth){
-        if(current == null){
-            return;
+    // private void dfs(TreeNode current, int depth){
+    //     if(current == null){
+    //         return;
+    //     }
+    //     if(depth>maxDepth){
+    //         // if true, we discovered a new level
+    //         maxDepth = depth;
+    //         bottomLeftValue =current.val;
+    //     }
+    //     dfs(current.left, depth+1);
+    //     dfs(current.right, depth+1);
+    
+
+    // BFS
+   public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        TreeNode current = root;
+        queue.add(current);
+
+        while (!queue.isEmpty()) {
+            current = queue.poll();
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+            if (current.left != null) {
+                queue.add(current.left);
+            }
         }
-        if(depth>maxDepth){
-            // if true, we discovered a new level
-            maxDepth = depth;
-            bottomLeftValue =current.val;
-        }
-        dfs(current.left, depth+1);
-        dfs(current.right, depth+1);
+        return current.val;
     }
 }
