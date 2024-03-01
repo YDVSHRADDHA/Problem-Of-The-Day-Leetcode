653cd5ba950fdf4e943a91c8767f05f3691e7877
Shraddha Yadav <97733987+YDVSHRADDHA@users.noreply.github.com>
2024-03-01 13:23:48 +0530

    Time: 3 ms (52.17%) | Memory: 43 MB (85.23%) - LeetSync

---
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    // Approach 1: Greedy Bit Manipulation (Sorting and Swapping)
+
+    public String maximumOddBinaryNumber(String s) {
+        char[] arr = s.toCharArray();
+        int N = arr.length;
+
+        Arrays.sort(arr);
+
+        int secondLast = N-2;
+        for(int i=0; i < N/2; i++){
+
+            char temp = arr[i];
+            arr[i] = arr[secondLast-i];
+            arr[secondLast-i] = temp;
+
+        }
+        return new String(arr);
+    }
+    
+}
