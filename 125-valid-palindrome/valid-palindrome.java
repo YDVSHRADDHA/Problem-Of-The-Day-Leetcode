1deded313f4e9bb96ead70a5c906740da59305e8
Shraddha Yadav <97733987+YDVSHRADDHA@users.noreply.github.com>
2025-03-30 23:30:11 +0530

    Time: 2 ms (99.09%) | Memory: 43 MB (76.63%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,31 +1,57 @@
 class Solution {
     public boolean isPalindrome(String s) {
-       // Convert the entire string to lowercase
-        s = s.toLowerCase();
-        // Convert the string to a character array
-        char[] charArray = s.toCharArray();
-        
-        int left = 0;
-        int right = charArray.length - 1;
-
+ 
+        int left = 0, right = s.length() - 1;
         while (left < right) {
-            // Skip non-alphanumeric characters
-            while (left < right && !Character.isLetterOrDigit(charArray[left])) {
-                left++;
-            }
-            while (left < right && !Character.isLetterOrDigit(charArray[right])) {
-                right--;
-            }
-            // Compare characters at left and right pointers
-            if (charArray[left] != charArray[right]) {
-                return false;
-            }
-            // Move both pointers towards the center
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
             left++;
             right--;
         }
-        
         return true;
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //    // Convert the entire string to lowercase
+    //     s = s.toLowerCase();
+    //     // Convert the string to a character array
+    //     char[] charArray = s.toCharArray();
+        
+    //     int left = 0;
+    //     int right = charArray.length - 1;
+
+    //     while (left < right) {
+    //         // Skip non-alphanumeric characters
+    //         while (left < right && !Character.isLetterOrDigit(charArray[left])) {
+    //             left++;
+    //         }
+    //         while (left < right && !Character.isLetterOrDigit(charArray[right])) {
+    //             right--;
+    //         }
+    //         // Compare characters at left and right pointers
+    //         if (charArray[left] != charArray[right]) {
+    //             return false;
+    //         }
+    //         // Move both pointers towards the center
+    //         left++;
+    //         right--;
+    //     }
+        
+    //     return true;
     
     }
 }
