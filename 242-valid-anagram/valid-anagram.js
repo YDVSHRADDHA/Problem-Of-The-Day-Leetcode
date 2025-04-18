dad429e15490d96ef44e82ebe2687c1fa8cd2fcc
Shraddha Yadav <97733987+YDVSHRADDHA@users.noreply.github.com>
2025-04-18 13:33:02 +0530

    Time: 4 ms (98.47%) | Memory: 56.6 MB (80.19%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    
+    let count = new Array(26).fill(0); // Array to store the character count
+    
+    for (let i = 0; i < s.length; i++) {
+        count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+    }
+    
+    return count.every(c => c === 0);
+};
