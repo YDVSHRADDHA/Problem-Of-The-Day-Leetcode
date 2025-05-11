fc504b651e4a7e752ee262298bb2039480069c30
Shraddha Yadav <97733987+YDVSHRADDHA@users.noreply.github.com>
2025-05-11 17:32:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (55.09%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.java
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -1,23 +1,33 @@
-
-class Solution {
+ class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        int cnt =0;
-        for(int i=0; i<arr.length;i++){
-            if(arr[i] % 2 != 0){
-             cnt++;
-              if(cnt>=3){
-            return true;
-        }
-
-            }
-            else{
-                cnt =0;
+        for (int i = 0; i < arr.length - 2; i++) {
+            if (arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0) {
+                return true;
             }
         }
-        if(cnt>=3){
-            return true;
-        }
-
         return false;
     }
 }
+
+// class Solution {
+//     public boolean threeConsecutiveOdds(int[] arr) {
+//         int cnt =0;
+//         for(int i=0; i<arr.length;i++){
+//             if(arr[i] % 2 != 0){
+//              cnt++;
+//               if(cnt>=3){
+//             return true;
+//         }
+
+//             }
+//             else{
+//                 cnt =0;
+//             }
+//         }
+//         if(cnt>=3){
+//             return true;
+//         }
+
+//         return false;
+//     }
+// }
