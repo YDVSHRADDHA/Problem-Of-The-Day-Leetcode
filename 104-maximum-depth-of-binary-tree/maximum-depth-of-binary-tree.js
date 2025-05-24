8c100a99d6ed70f0877e49d119f9a910d5a84905
Shraddha Yadav <97733987+YDVSHRADDHA@users.noreply.github.com>
2025-05-24 17:26:57 +0530

    Time: 0 ms (100.00%) | Memory: 58.3 MB (40.82%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -1,8 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val);
+ *     this.left = (left===undefined ? null : left);
+ *     this.right = (right===undefined ? null : right);
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
 var maxDepth = function(root) {
-    if (!root) return 0;
+    if (root === null) return 0; // no tree => depth 0
 
-    const leftDepth = maxDepth(root.left);
-    const rightDepth = maxDepth(root.right);
-
-    return Math.max(leftDepth, rightDepth) + 1;
+    // Recursively find max depth of left and right subtree and add 1 for current node
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 };
